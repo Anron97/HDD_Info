4f7a7daa5046463e053901fc3d5445099db62712
Andrei Shakarov <andrei.shakarov@gmail.com>
2017-10-07 14:03:00 +0300

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import main.HardDiskDrive.HardDiskDrive;
+
+public class Main {
+    public static void main(String[] args) {
+        HardDiskDrive hardDiskDrive = new HardDiskDrive();
+        System.out.println(hardDiskDrive);
+
+    }
+}
